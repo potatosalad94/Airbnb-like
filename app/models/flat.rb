503c8aa32e5a49a605d456f32331c4479b9b4c9a
remi achji <achji.remi@gmail.com>
2020-02-03 17:55:32 +0400

    added index content, cloudinary service, image mount for flats and users

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,6 +1,7 @@
 class Flat < ApplicationRecord
   has_many   :bookings
   belongs_to :user
+  has_one_attached :photo
   validates  :name, presence: true, uniqueness: { scope: :address }
   validates  :city, presence: true
   validates  :address, presence: true
