baa0ddc43c2bf9d8811bcefe28ba3987e65c6c12
remi achji <achji.remi@gmail.com>
2020-02-03 15:07:52 +0400

    added user model and validations on first name last name and email

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
