89876de34dbd796e1d4a1ed16547e8086743fcde
remi achji <achji.remi@gmail.com>
2020-02-05 15:31:30 +0400

    saving progress

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,14 +1,22 @@
 class BookingsController < ApplicationController
   def index
+    @bookings = Booking.all
   end
 
   def show
   end
 
-  def new
-  end
-
   def create
+    @flat = Flat.find(params[:flat_id])
+    @booking = Booking.new(booking_params)
+    @booking.flat = @flat
+    @booking.user = current_user
+    if @booking.save
+      redirect_to flats_path
+    else
+      raise
+      render "flats/show"
+    end
   end
 
   def edit
@@ -19,4 +27,11 @@ class BookingsController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :number_of_guests)
+  end
+
 end
--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -7,6 +7,7 @@ class FlatsController < ApplicationController
   end
 
   def show
+    @booking = Booking.new
   end
 
   def new
