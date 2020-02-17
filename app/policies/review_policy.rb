61411a1fcb515b6cb60ef61a2c91406716b76777
remi achji <achji.remi@gmail.com>
2020-02-17 10:08:31 +0400

    saving

--- app/policies/review_policy.rb
+++ app/policies/review_policy.rb
@@ -7,6 +7,6 @@ class ReviewPolicy < ApplicationPolicy
 
   def create?
     # verifier que l'utilisateur a deja fait un booking dans ce flat
-    user.bookings.include?(@flat)
+    user.bookings.include?(@flat) || user.admin
   end
 end
