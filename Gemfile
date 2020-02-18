f77f389f767fa8d31cfd28ed108942f370776c85
remi achji <achji.remi@gmail.com>
2020-02-18 12:35:45 +0400

    saved

--- Gemfile
+++ Gemfile
@@ -19,6 +19,7 @@ gem 'cloudinary', '~> 1.12.0'
 gem 'money-rails', '~>1.12'
 gem 'pundit'
 gem 'geocoder'
+gem 'activeadmin'
 
 group :development do
   gem 'web-console', '>= 3.3.0'
---
+++ app/admin/bookings.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Booking do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  permit_params :start_date, :end_date, :number_of_guests, :flat_id, :user_id, :status
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:start_date, :end_date, :number_of_guests, :flat_id, :user_id, :status]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+
+end
